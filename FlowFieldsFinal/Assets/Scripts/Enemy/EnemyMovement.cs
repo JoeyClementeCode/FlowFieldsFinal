c1257c5e35161597e9a42701a519f2e5e3d2fb14
Jeffrey <jeffrey.popek@gmail.com>
2023-11-02 23:57:02 -0400

    Sprint 1 - Basic Enemy
    - Created initial enemy prefab

---
+++ FlowFieldsFinal/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private float movementSpeed = 7f;
+
+    //private Vector3 movementDirection = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
